b91bf9a122a2eb3c674da65bbb735f73dfc753e7
Tim Wilson <tim@twilson.net>
2023-10-26 23:32:47 -0400

    Improve default fragment shader

--- src/shaders/defaultFragment.glsl.ts
+++ src/shaders/defaultFragment.glsl.ts
@@ -7,6 +7,6 @@ export const defaultFragment = `
     void main() 
     {
         vec2 uv = gl_FragCoord.xy / iResolution.xy;
-        gl_FragColor = vec4(iTime + .5 + .5 * cos(uv.xyx + vec3(0., 2., 4.)), 1.);
+        gl_FragColor = vec4(.5 + .5 * cos(iTime + uv.xyx + vec3(0., 2., 4.)), 1.);
     }
 `;
